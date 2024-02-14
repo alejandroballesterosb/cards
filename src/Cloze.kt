a9c5b82f6adfa84f06d88d3d4310152b0944958c
alejandroballesterosb <alejandro.ballesterosb@estudiante.uam.es>
2024-02-14 14:26:21 +0100

    Mejor hecho el override de show

--- src/Cloze.kt
+++ src/Cloze.kt
@@ -3,18 +3,10 @@ class Cloze (
     answer: String) : Card(question, answer){
 
     override fun show(){
-        print(" $question (ENTER to see answer)")
-        readln()
-
         val replacedAnswer = question.replace(Regex("\\*(.*?)\\*")) {answer}
-
-        print(" $replacedAnswer (Type 0 -> Difficult 3 -> Doubt 5 -> Easy): ")
-        do {
-            quality = readln().toIntOrNull()
-            if(quality != 0 && quality != 3 && quality != 5){
-                print("Please, try again (Type 0 -> Difficult 3 -> Doubt 5 -> Easy): ")
-            }
-        } while (quality != 0 && quality != 3 && quality != 5)
-        update(nextPracticeDate)
+        val answeraux = answer
+        answer = replacedAnswer
+        super.show()
+        answer = answeraux
     }
 }
